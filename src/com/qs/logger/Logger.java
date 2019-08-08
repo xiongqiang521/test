8b0c1a101085c6b83ceb3751d67913fabf157a5a
XQ <1379716219@qq.com>
2019-08-08 12:50:29 +0800

    ssh整合项目初始提交

--- src/com/qs/logger/Logger.java
+++ src/com/qs/logger/Logger.java
@@ -1,7 +1,33 @@
 package com.qs.logger;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 public class Logger {
     public void printLog(){
         System.out.println("增强代码-------");
     }
+
+    public Object around(ProceedingJoinPoint point){
+        Object re=null;
+
+        Object[] args = point.getArgs();//获取参数列表
+
+        try {
+            System.out.println("前置通知");
+
+            re = point.proceed(args);//执行的方法，需要增强的方法
+            System.out.println("后置通知");
+
+
+        } catch (Throwable throwable) {
+            System.out.println("异常通知");
+
+            throwable.printStackTrace();
+        }finally {
+            System.out.println("最终通知");
+
+        }
+        return re;
+    }
+
 }
